bc471ea8f84b464bf8d0b33c876ebd8d4fc3770b
makaota <74915165+makaota@users.noreply.github.com>
2022-06-05 14:41:52 +0200

    Initial commit

---
+++ app/src/main/java/android/app/jetpackcomposemeditationui/MainActivity.kt
@@ -0,0 +1,43 @@
+package android.app.jetpackcomposemeditationui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import android.app.jetpackcomposemeditationui.ui.theme.JetpackComposeMeditationUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeMeditationUITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeMeditationUITheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/android/app/jetpackcomposemeditationui/ui/theme/Theme.kt
@@ -0,0 +1,16 @@
+package android.app.jetpackcomposemeditationui.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+@Composable
+fun JetpackComposeMeditationUITheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    MaterialTheme(
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
