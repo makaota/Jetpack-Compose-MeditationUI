9205e149f66bf1a382bd41a423d3321ed4fdd508
makaota <74915165+makaota@users.noreply.github.com>
2022-06-05 20:24:59 +0200

    Feature Section added in home screen after current meditation function

--- app/src/main/java/android/app/jetpackcomposemeditationui/ui/theme/HomeScreen.kt
+++ app/src/main/java/android/app/jetpackcomposemeditationui/ui/theme/HomeScreen.kt
@@ -1,10 +1,16 @@
 package android.app.jetpackcomposemeditationui.ui.theme
 
+import android.app.jetpackcomposemeditationui.Feature
 import android.app.jetpackcomposemeditationui.R
+import android.app.jetpackcomposemeditationui.standardQuadFromTo
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
@@ -13,10 +19,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
@@ -30,6 +41,36 @@ fun HomeScreen() {
             GreetingSection()
             ChipSection(chips = listOf("Sweet sleep", "Insomnia", "Depression"))
             CurrentMeditation()
+            FeatureSection(features = listOf(
+                Feature(
+                    title = "Sleep meditation",
+                    R.drawable.ic_headphone,
+                    BlueViolet1,
+                    BlueViolet2,
+                    BlueViolet3
+                ),
+                Feature(
+                    title = "Tips for sleeping",
+                    R.drawable.ic_videocam,
+                    LightGreen1,
+                    LightGreen2,
+                    LightGreen3
+                ),
+                Feature(
+                    title = "Night island",
+                    R.drawable.ic_headphone,
+                    OrangeYellow1,
+                    OrangeYellow2,
+                    OrangeYellow3
+                ),
+                Feature(
+                    title = "Calming sounds",
+                    R.drawable.ic_headphone,
+                    Beige1,
+                    Beige2,
+                    Beige3
+                )
+            ))
         }
     }
 }
@@ -148,7 +189,124 @@ fun CurrentMeditation(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun FeatureSection(){
+fun FeatureSection(features: List<Feature>) {
 
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = "Feature",
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier.padding(15.dp)
+        )
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(2),
+            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            items(features.size) {
+                FeatureItem(feature = features[it])
+
+            }
+        }
+    }
+}
+
+@Composable
+fun FeatureItem(
+    feature: Feature
+) {
+    BoxWithConstraints(
+        modifier = Modifier
+            .padding(7.5.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .background(feature.darkColor)
+    ) {
+
+        val width = constraints.maxWidth
+        val height = constraints.maxHeight
+
+        // Medium colored path
+        val mediumColorPont1 = Offset(0f, height * 0.3f)
+        val mediumColorPont2 = Offset(0f, height * 0.3f)
+        val mediumColorPont3 = Offset(0f, height * 0.3f)
+        val mediumColorPont4 = Offset(0f, height * 0.3f)
+        val mediumColorPont5 = Offset(0f, height * 0.3f)
+
+        val mediumColoredPath = Path().apply {
+            moveTo(mediumColorPont1.x, mediumColorPont1.y)
+            standardQuadFromTo(mediumColorPont1, mediumColorPont2)
+            standardQuadFromTo(mediumColorPont2, mediumColorPont3)
+            standardQuadFromTo(mediumColorPont3, mediumColorPont4)
+            standardQuadFromTo(mediumColorPont4, mediumColorPont5)
+            lineTo(width.toFloat() * 100f, height.toFloat() * 100f)
+            lineTo(-100f, height.toFloat() * 100f)
+            close()
+
+        }
+
+        // Light colored path
+        val lightPoint1 = Offset(0f, height * 0.35f)
+        val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
+        val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
+        val lightPoint4 = Offset(width * 0.65f, height.toFloat())
+        val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 3f)
+
+        val lightColoredPath = Path().apply {
+            moveTo(lightPoint1.x, lightPoint1.y)
+            standardQuadFromTo(lightPoint1, lightPoint2)
+            standardQuadFromTo(lightPoint2, lightPoint3)
+            standardQuadFromTo(lightPoint3, lightPoint4)
+            standardQuadFromTo(lightPoint4, lightPoint5)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+        Canvas(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            drawPath(
+                path = mediumColoredPath,
+                color = feature.mediumColor
+            )
+            drawPath(
+                path = lightColoredPath,
+                color = feature.lightColor
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(15.dp)
+        ) {
+            Text(
+                text = feature.title,
+                style = MaterialTheme.typography.h1,
+                lineHeight = 26.sp,
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+            )
+            Icon(
+                painter = painterResource(id = feature.iconId),
+                contentDescription = feature.title,
+                tint = Color.White,
+                modifier = Modifier.align(Alignment.BottomStart)
+            )
+            Text(
+                text = "Start",
+                color = TextWhite,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .clickable {
+                        // Handle the click
+                    }
+                    .align(Alignment.BottomEnd)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(ButtonBlue)
+                    .padding(vertical = 6.dp, horizontal = 15.dp)
+            )
+        }
+    }
 }
